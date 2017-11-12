3a94cad56e4522e0ba11a7bc18a8c894822383ef
Michael McCulloch <michael.james.mcculloch@gmail.com>
2017-11-12 15:23:19 -0700

    Skeleton

---
+++ src/ConcurrentPriorityQueue.java
@@ -0,0 +1,10 @@
+public class ConccurrentPriorityQueue<T>{
+
+    /**
+     * private T t;
+     * public void set(T t) {this.t = t;}
+     * public T get() {return t}
+     */
+
+
+}
---
+++ src/Searcher.java
@@ -0,0 +1,11 @@
+
+public class Searcher {
+    
+    public Searcher() {
+        Concu
+    }
+
+    public static void main(String[] args){
+        System.out.println("TEST");
+    }
+}
