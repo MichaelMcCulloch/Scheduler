118500e5d089da5e25a9a83ab68708ca70e01fc7
Michael McCulloch <michael.james.mcculloch@gmail.com>
2017-11-13 11:56:07 -0700

    Add more documentation

--- src/Searcher.java
+++ src/Searcher.java
@@ -1,5 +1,15 @@
 import java.util.concurrent.PriorityBlockingQueue;
-
+/**
+ * Searcher
+ * Idea is that many of these can run simultaneously, and share a work queue. 
+ * While goal state not met:
+    *  Remove a PROB K from the queue,
+    *  ...some processing tbd
+    *  Remove a DIV D from K.divisons queue
+    *  ks = DIV(K)
+    *  ...some processing tbd
+    *  put all ks into the work queue;
+ */
 public class Searcher implements Runnable {
     
     public PriorityBlockingQueue<Node<Prob>> qRef;
